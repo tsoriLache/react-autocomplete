dfe0de33766e26c43f156ca54c0ec819e52b1b02
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-22 23:40:05 +0200

    data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { OptionsDropdown } from './components/OptionsDropdown';
 import { SearchBar } from './components/SearchBar.jsx';
+import countries from './data';
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
-      <OptionsDropdown countries={['A', 'B', 'C']} />
+      <OptionsDropdown countries={countries} />
     </div>
   );
 }
